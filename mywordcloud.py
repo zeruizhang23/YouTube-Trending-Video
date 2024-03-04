6862a11a2a540aafaf66a1a7fb1c10879e9eb436
Shiyi Chen <107733060+cooooozi@users.noreply.github.com>
2024-03-04 14:31:14 -0800

    Update mywordcloud.py For Integration

--- mywordcloud.py
+++ mywordcloud.py
@@ -9,8 +9,8 @@ from io import BytesIO
 import base64
 
 # Ensure NLTK resources are downloaded
-nltk.download('stopwords')
-nltk.download('wordnet')
+nltk.download('stopwords', quiet=True)
+nltk.download('wordnet', quiet= True)
 
 def clean_tokenize(text, stop_words):
     text = text.lower()
@@ -20,9 +20,7 @@ def clean_tokenize(text, stop_words):
     words = [word for word in words if len(word) > 2 and not any(char.isdigit() for char in word)]
     return words
 
-def generate_wordcloud(selected_country):
-    # Load and prepare data inside the function to avoid using global variables
-    df = pd.read_csv('YouTube-Trending-Video.csv')
+def generate_wordcloud(df, selected_country, width, height):
     additional_stopwords = set(['-', '|', 'official', 'trailer', '2023', '2024', 'video']) 
     stop_words = set(stopwords.words('english')) | additional_stopwords
 
@@ -32,17 +30,10 @@ def generate_wordcloud(selected_country):
     words = clean_tokenize(text, stop_words)
     word_counts = Counter(words)
     
-    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_counts)
+    wordcloud = WordCloud(width=width, height=height, background_color='white').generate_from_frequencies(word_counts)
     img = wordcloud.to_image()
     with BytesIO() as buffer:
         img.save(buffer, 'png')
         img_bytes = buffer.getvalue()
     img_base64 = base64.b64encode(img_bytes).decode()
     return f'data:image/png;base64,{img_base64}'
-
-
-
-
-
-
-
