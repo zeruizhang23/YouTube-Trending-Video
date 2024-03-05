861b9a422102d7c3c93c1629248144be4180ecfa
陈诗怡 <chenshiyi@MacBook-Air.local>
2024-03-04 17:49:13 -0800

    add sidebar

--- Top_5_videos.py
+++ Top_5_videos.py
@@ -10,14 +10,15 @@ def generate_top5_videos(df, selected_country='US'):
     rows = []
     for _, row in top_5videos.iterrows():
         video_info = dbc.Col([
-            html.H5(row['title'], className='mt-0'),
-            html.P(f"Views: {row['view_count']}   Comments: {row['comment_count']}", style={'margin-bottom': '2px'}),
-            html.P(f"Likes: {row['likes']}   Dislikes: {row['dislikes']}", style={'margin-bottom': '2px'})
-        ], width=8)
+            html.H6(row['title'], className='mt-2', style={'white-space': 'normal'}),
+            html.P(f"Views: {row['view_count']}", style={'margin-bottom': '2px', 'white-space': 'normal'}),
+            html.P(f"Comments: {row['comment_count']}", style={'margin-bottom': '2px', 'white-space': 'normal'}),
+            html.P(f"Likes: {row['likes']}", style={'margin-bottom': '2px', 'white-space': 'normal'})
+        ], width=6, style={'padding-right': '10px'})
 
         thumbnail = dbc.Col([
-            html.Img(src=row['thumbnail_link'], style={'width': '100px', 'height': 'auto'})
-        ], width=2)
+            html.Img(src=row['thumbnail_link'], style={'width': '180px', 'height': 'auto','display': 'block', 'margin-left': 'auto', 'margin-right': 'auto'})
+        ], width=6)
 
         video_row = dbc.Row([thumbnail, video_info], className='mb-3')
         rows.append(video_row)
